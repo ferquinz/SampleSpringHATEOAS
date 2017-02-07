226ecbb1a250a392cc10228c164d54f172a3d825
ferquinz <ferquinz10@gmail.com>
2017-02-07 13:50:22 +0100

    Finished example with Mockito test

--- src/test/java/com/example/SampleSpringHateoasApplicationTests.java
+++ src/test/java/com/example/SampleSpringHateoasApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example;
 
+import com.example.hello.GreetingController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class SampleSpringHateoasApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	GreetingController greetingController;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(greetingController).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void defaultValue() throws Exception {
+		this.mockMvc.perform(get("/greeting?name={name}", "World")).andExpect(status().isOk());
+		//assertTrue(greetingController.greeting("").toString().contains("World"));
 	}
 
 }
